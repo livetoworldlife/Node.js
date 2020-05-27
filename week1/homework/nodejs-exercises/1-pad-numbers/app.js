5ef279dba8b8515d00ec105aaad9241f970177c3
livetoworldlife <55538102+livetoworldlife@users.noreply.github.com>
2020-05-27 23:26:43 +0200

    cleared unnecessary line

--- week1/homework/nodejs-exercises/1-pad-numbers/app.js
+++ week1/homework/nodejs-exercises/1-pad-numbers/app.js
@@ -4,8 +4,4 @@ const padLeft = require("./padLeft");
 
 const numbers = ["12", "846", "2", "1236"];
 
-numbers.forEach((number, index) => {
-
-  let result = padLeft.padLeft(number, 5, "_");
-  console.log(result);
-})
+numbers.forEach(number=> console.log(padLeft.padLeft(number, 5, "_")));

c403ca6c2bc0f0facbe3b76bcc3d5c01cd6a9273
livetoworldlife <55538102+livetoworldlife@users.noreply.github.com>
2020-06-09 16:56:38 +0200

    added post route

--- week2/homework/hackyourtemperature2/server.js
+++ week2/homework/hackyourtemperature2/server.js
@@ -1,6 +1,5 @@
 'use strict';
 // https://quiet-lowlands-26235.herokuapp.com/
-
 const axios = require('axios'),
   exphbs = require('express-handlebars'),
   express = require('express'),
@@ -11,11 +10,18 @@ const axios = require('axios'),
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Make a GET request to / that sends the message
 app.get('/', (request, response) => {
   response.render('index');
 })
+// Create a POST route, that has as an endpoint: /weather
+app.post('/weather', (request, response) => {
+  const cityName = request.body.cityName;
+  response.send(cityName);
+});
 
-//listen to port 
+//listen to port 3000
 app.listen(PORT, () => console.log(`Server is running on ${PORT}...`)); 
